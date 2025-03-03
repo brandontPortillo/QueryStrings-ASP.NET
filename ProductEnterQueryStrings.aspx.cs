8301f0afbc487a144310e66db4330c7cd9a30dc5
brandontPortillo <5834255@clases.edu.sv>
2025-03-03 14:51:55 -0600

    Primer commit (sólo UI)

---
+++ ProductEnterQueryStrings.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QueryString5834255
+{
+    public partial class ProductEnterQueryStrings : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
